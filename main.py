1ac56dc599307f6325e6b682651739ef5a6d6be6
Dixon Marino <dmusicmsoccer14@gmail.com>
2025-11-16 11:19:56 -0500

    uploading my profile

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from profile-07!")
+from fastapi import FastAPI
+import requests, json
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+url = 'https://api.github.com/users/Dixon07Marino'
+url_ = 'https://api.github.com/users/Dixon07Marino/repos'
+
+@app.get("/api/profile/Dixon07Marino")
+async def get_profile():
+    response = requests.get(url).json()
+    with open('profile.json', 'w') as f:
+        f.write(json.dumps(response, indent=4))
+        return {"message": "Profile data saved to profile.json"}
+    
+@app.get("/api/repos/Dixon07Marino")
+async def get_repos():
+    response = requests.get(url_).json()
+    with open('repos.json', 'w') as f:
+        f.write(json.dumps(response, indent=4))
+        return {"message": "Repos data saved to repos.json"}
